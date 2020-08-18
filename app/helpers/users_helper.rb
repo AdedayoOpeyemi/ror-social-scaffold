56b94f7b391dd1b902897b566125a69e897396d5
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2020-08-18 19:03:55 +0100

    modified conditonal

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,9 @@
 module UsersHelper
   def friendship_request_link(user)
     not_friend = current_user.not_friend_with?(user)
-    
-    link_to 'Request Friendship', request_friendship_url(user), class: 'profile-link', method: :post if not_friend && current_user != user
+
+    return unless not_friend && current_user != user
+
+    link_to 'Request Friendship', request_friendship_url(user), class: 'profile-link', method: :post
   end
 end
